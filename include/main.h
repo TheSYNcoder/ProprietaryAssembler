915a9460c707985a3cec5d02af8416ed089bfbe2
theSYNcoder <papaigd@gmail.com>
2021-01-04 23:26:42 +0530

    convert int8_t to char

--- include/main.h
+++ include/main.h
@@ -10,10 +10,10 @@ Necessary structs defined here
 typedef struct op_tab_node{
 
     int32_t sl_no; /* serial number of opcode */
-    int8_t symbol[10]; /* The mnemonic such as ADD, SUB */
-    int8_t opcode[5]; /* The opcode referring to the mnemonic */
+    char symbol[10]; /* The mnemonic such as ADD, SUB */
+    char opcode[5]; /* The opcode referring to the mnemonic */
     int32_t length; /* The length of the instruction */
-    int8_t add_mode[20]; /* the addressing mode - relative, absolute etc */
+    char add_mode[20]; /* the addressing mode - relative, absolute etc */
 
 }op_tab_node;
 
