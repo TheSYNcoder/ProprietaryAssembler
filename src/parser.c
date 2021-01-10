0d3ec9f18b1eb91e44b55ec178227834c738c3cb
Soumalya1857 <soumalyakundu10@gmail.com>
2021-01-10 15:15:06 +0530

    Parser added

--- src/parser.c
+++ src/parser.c
@@ -51,6 +51,30 @@ int validate(char ch)
         return 0;
     }
    
+void add_and_reset_lines(Token* temp, Line* line)
+{
+    line->tokens[line->length] = *temp;
+    line->length++;
+
+    //printLine(line);
+
+    /*call here suvyan and aritra*/
+    //op_tab_node* op_tab = call_fucking_shuvyan(line);
+    //hello_aritra_samanta(op_tab);
+
+    temp->length = 0;
+    for(int k=0;k<10;k++) temp->word[k] = '\0';
+    // free(temp.word);
+    // temp.word = (char*)malloc(10*sizeof(char));
+
+    for (int k=0;k<line->length;k++)
+    {
+        line->tokens[k] = *temp;
+    }
+    line->length = 0;
+}
+
+
 
 void parse(char* fileName)
 {
@@ -83,11 +107,6 @@ void parse(char* fileName)
             // printf("%c\n",ch);
             if(ch != '\n' && ch != ' ' && ch != ';' && ch != '\t')
             {
-                if(temp.length >= 10)
-                {
-                    //temp.word = (char*)realloc(temp.word, (temp.length+1)*sizeof(char));
-                    /*error not handled here*/
-                }
                 temp.word[temp.length] = ch;
                 temp.length++;
             }
@@ -96,96 +115,39 @@ void parse(char* fileName)
                 if(ch == ' ' || ch == ',' || ch == '\t')
                 {
                     /*ignore all the white space untill you get a char*/
-                    while(fread(&buffer,sizeof(char),1,fptr))
+                    while(fread(&ch,sizeof(char),1,fptr))
                     {
-                        if(buffer != '\n' && buffer != ' ' && buffer != ';')
+                        if(ch != '\n' && ch != ' ' && ch != ';' && ch != '\t')
                         {
                             fseek(fptr, -sizeof(char), SEEK_CUR);
                             /*add the token to line and set Token temp to initial value*/
-                            //if(currentLine.length >= 3) currentLine.tokens = (Token*)realloc(currentLine.tokens, sizeof(currentLine)+sizeof(temp));
                             currentLine.tokens[currentLine.length] = temp;
-                            /**upto this working*/
                             currentLine.length++;
 
-                            printf("Boom1space\n");
-                            printLine(&currentLine);
-                            printf("Boom2space\n");
 
                             /*set temp to original value*/
                             temp.length = 0;
                             for(int k=0;k<10;k++) temp.word[k] = '\0';
                             break;
+
                         }
                         /* but if you get a newline or comment*/
                         /*add your current Token to Line and call fucking Shuvyan*/
-                        else if(buffer == '\n' || buffer == ';')
+                        else if(ch == '\n' || ch == ';')
                         {  
                             /*add the token to line*/ 
-                            if(currentLine.length == 3)
-                            {
-                                fprintf(stderr,"Parser error! Too much argumnet in a line!!");
-                                exit(2);
-                            }
-                            currentLine.tokens[currentLine.length] = temp;
-                           
-                           
-                            currentLine.length++;
-                            
-                            /*sending ref of Line...receiving ref of op_tab_node*/
-                            // op_tab_node* op_tab = call_fucking_shuvyan(&currentLine);
-                            // hello_aritra(op_tab); 
-
-
-                            /*set temp and currentLine to to original value*/
-                            temp.length = 0;
-                            for(int k=0;k<10;k++) temp.word[k] = '\0';
-
-
-                            for (int k=0;k<currentLine.length;k++)
-                            {
-                                currentLine.tokens[k] = temp;
-                            }
-                            currentLine.length = 0;
-                            
+                            add_and_reset_lines(&temp, &currentLine);
                             break;
                         }
                         else
                         {
                             continue;
-                        }
-                        
-                        
+                        }   
                     }
-                    
-
                 }
-                else if(buffer == '\n' || buffer == ';')
+                else if(ch == '\n' || ch == ';')
                 {
-                        /*add the token to line*/ 
-                        //printf("BOOM BABY!\n");
-                        //if(currentLine.length >= 3) currentLine.tokens = (Token*)realloc(currentLine.tokens, sizeof(currentLine)+sizeof(temp));
-                        currentLine.tokens[currentLine.length] = temp;
-                        currentLine.length++;
-
-                        /*sending ref of Line...receiving ref of op_tab_node*/
-                        // op_tab_node* op_tab = call_fucking_shuvyan(&currentLine);
-                        // hello_aritra(op_tab);
-
-
-                        /*set temp and currentLine to to original value*/
-                        temp.length = 0;
-                        for(int k=0;k<10;k++) temp.word[k] = '\0';
-                        // free(temp.word);
-                        // temp.word = (char*)malloc(10*sizeof(char));
-
-                        for (int k=0;k<currentLine.length;k++)
-                        {
-                            currentLine.tokens[k] = temp;
-                        }
-                        currentLine.length = 0;
-                        // free(currentLine.tokens);
-                        // currentLine.tokens =  (Token*)malloc(3*sizeof(Token));
-                        break;
+                        add_and_reset_lines(&temp, &currentLine);
                 }
             }
         }
